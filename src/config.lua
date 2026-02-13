c5ec828de6d4beb3b9ac0b66baa876ca659aaa80
BlackJackalGaming <blackjackalgamingfb@gmail.com>
2026-02-13 11:35:54 -0500

    added more

--- src/config.lua
+++ src/config.lua
@@ -2,13 +2,17 @@
 local config = {
     Enabled = true,
     Debug = false,
-    RGBOpts = {}
+    Intensity = 
+    RGBOpts = {  -- PhaseT, Speed, frameTime, etc.
+        
+    }
 
 }
 
 local configDescription = {
     Enabled = "Enable RGB Text Generator",
-    Debug = "Enable detailed debug logging to console (check ReturnOfModding/LogOutput.log)"
+    Debug = "Enable detailed debug logging to console (check ReturnOfModding/LogOutput.log)",
+    RGBOpts = "Options for fine-tuning RGB Text Generator behavior. Not currently implemented."
 }
 
 return config, configDescription
--- src/core.lua
+++ src/core.lua
@@ -1,11 +1,26 @@
 ---@meta _
 ---@diagnostic disable: spell-check
+---@
 local function ArePerfectModsAvailable()
     if not rom or not rom.mods then
         return false end
     return rom.mods['Jowday-BoonBuddy'] ~= nil
     and rom.mods['Jowday-Perfectoinist'] ~= nil
 end
+
+local perfectionColor = { 255, 255, 255, 255 } -- default color if perfection mods are not available
+if rom.mods['Jowday-Perfectoinist'] ~= nil then
+    game.Color.BoonPatchPerfect = { 97, 230, 255, 255 } -- is same call as in Perfectoinist mod, but we need to set it here to be able to use it in our code
+        if config.debug then
+            print("[RGBTextGenerator] Perfection mods found, using custom color for perfection text")
+        end
+    return perfectionColor = game.Color.BoonPatchPerfect
+else
+    if config.debug then
+        print("[RGBTextGenerator] Perfection mods not found, using default color for perfection text")
+    end
+end
+
 
 -- Mod initialization
 if config.debug then
